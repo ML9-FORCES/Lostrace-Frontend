fc60cd12cd0b17e79c5e982d32d357763ec072fd
Subin <subinsk284@gmail.com>
2022-03-09 19:13:20 +0530

    tailwind css intiated

---
+++ src/components/HomePage/TopBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../common/Logo'
+
+const TopBar = () => {
+    return (
+        <div className='flex border-2 border-blue-500 p-2'>
+            <Logo />
+        </div>
+    )
+}
+
+export default TopBar
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopBar from '../components/HomePage/TopBar'
+
+const Home = () => {
+    return (
+        <TopBar />
+    )
+}
+
+export default Home
