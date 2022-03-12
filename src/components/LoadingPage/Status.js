f45be1bf3078940f65af12a71f8d67ec5d793b3a
prateekgarg08 <prateekgarg881@gmail.com>
2022-03-12 22:04:51 +0530

    loading page first version

---
+++ src/components/LoadingPage/Status.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import styles from './Status.module.css'
+
+function Status({ status }) {
+    return (
+        <div className="flex justify-center items-center p-2">
+            <p className="text-black text-2xl font-bold">{status}</p>
+        </div>
+    )
+}
+
+export default Status
