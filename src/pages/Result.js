2cbea3a93737a5b0cdb784fcc889cb12ba8027a5
prateekgarg08 <prateekgarg881@gmail.com>
2022-03-13 19:12:18 +0530

    displayed data from api to page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import { fetchPerson } from '../services/fetchPerson'
+import img from "../images/temp.jpg"
 
 const Result = () => {
+    const [name, setName] = useState('');
+    const [fatherName, setFatherName] = useState('');
     const [currentAge, setCurrentAge] = useState('');
+    const [gender, setGender] = useState('');
+    // const [img, setImg] = useState('');
+    const [placeOfMissing, setPlaceOfMissing] = useState('');
+    const [reportLink, setReportLink] = useState('');
+    const [profileLink, setProfileLink] = useState('')
+
+
 
     useEffect(() => {
         fetchPerson()
             .then((res) => {
+                console.log(res.data)
                 setCurrentAge(res.data.Current_Age);
+                setName(res.data.Name)
+                setFatherName(res.data.Father_Name)
+                setGender(res.data.Gender)
+                // setImg(res.data.Img)
+                setPlaceOfMissing(res.data.Place_of_Missing)
+                setReportLink(res.data.Report_link)
+                setProfileLink(res.data.Profile_link)
             })
             .catch((err) => {
                 console.log(err)
@@ -16,7 +34,14 @@ const Result = () => {
 
     return (
         <div>
+            <p>Name: {name}</p>
+            <p>Father Name: {fatherName}</p>
+            <p>Gender: {gender}</p>
             <p>Current Age: {currentAge}</p>
+            <img src={img} />
+            <p>Place of missing : {placeOfMissing}</p>
+            <a href={reportLink}><button className='border border-white text-white bg-teal-800'>Report</button></a>
+            <a href={profileLink}><button className='border border-white text-white bg-teal-800'>Profile</button></a>
         </div>
     )
 }
