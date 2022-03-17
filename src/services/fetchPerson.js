cfa1312988c406aa0c07583451c0c27486746a36
Subin S K <subinsk284@gmail.com>
2022-03-17 18:45:39 +0530

    updated app

--- src/services/fetchPerson.js
+++ src/services/fetchPerson.js
@@ -1,11 +1,15 @@
 import axios from "axios"
 
-const config = {
-    method: 'get',
-    url: 'http://lostrace-data-api.herokuapp.com/result'
-}
+export const fetchPerson = (img) => {
+    const formData = new FormData();
+    formData.append("img", img);
+    formData.append("flag", '1');
+    const config = {
+        method: 'post',
+        url: 'http://lostrace-data-api.herokuapp.com/result',
+        data: formData
+    }
 
-export const fetchPerson = () => {
     try {
         const data = axios(config);
         return new Promise((res, rej) => {
